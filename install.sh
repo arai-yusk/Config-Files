55569881be11503f8c5da29698f82ee5043abccd
arai-yusk <riusk.0049@gmail.com>
2022-10-20 01:45:12 +0900

    不要コメントを削除、コメントの修正

--- install.sh
+++ install.sh
@@ -9,16 +9,13 @@ if ! [ -e ${HOME}/${DOTFILE_DIR}/${BACKUP_DIR} ]; then
 fi
 
 echo "move config files to ${DOTFILE_DIR} as backups."
-#cat ${PWD}/install_list.txt |
-#xargs -I@ \
-#  mv ${HOME}/.@ ~/${DOTFILE_DIR}/${BACKUP_DIR}/
 for f in $(cat ${PWD}/install_list.txt); do
   if [ -e ${HOME}/.${f} ]; then
     mv ${HOME}/.${f} ~/${DOTFILE_DIR}/${BACKUP_DIR}/.${f}
   fi
 done
 
-echo "make configs file in home directory as symbolic links."
+echo "make config files in home directory as symbolic links."
 for f in $(cat ${PWD}/install_list.txt); do
   ln -s ${PWD}/${f} ${HOME}/.${f}
 done
